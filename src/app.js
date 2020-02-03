64929849cce57c94db1c2124bbba36c02e904edf
Angus McNair <amcnair234@gmail.com>
2020-02-02 19:09:32 -0600

    CORS revision

--- src/app.js
+++ src/app.js
@@ -18,11 +18,7 @@ const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
   app.use(morgan(morganSetting))
   app.use(helmet())
   
-app.use(
-  cors({
-      origin: CLIENT_ORIGIN
-  })
-);
+app.use(cors());
 
 
     app.use('/api/ailments', ailmentsRouter)
