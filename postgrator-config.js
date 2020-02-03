ebc64a0e95a29e4dd8ec7744fd92765c331d308e
Angus McNair <amcnair234@gmail.com>
2020-02-02 20:44:43 -0600

    database and table edits `

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,5 +5,7 @@ module.exports = {
   "driver": "pg",
   "port": 4000,
   "connectionString": (process.env.NODE_ENV === 'test')
-  ? process.env.TEST_DB_URL
-  : process.env.DB_URL, }
+  ? process.env.TEST_DATABASE_URL
+  : process.env.DATABASE_URL, 
+  "ssl": !!process.env.SSL,
+}
--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     API_ENDPOINT: 'http://localhost:8000/api',
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://grampsmd@localhost/grampsmd',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/blogful',
     CLIENT_ORIGIN: 'https://grampsmdclient-htzi5p41n.now.sh/'
   }
