e7edd662328799326379cc7cc72fcf591ccf8383
gusmcnair <56044029+gusmcnair@users.noreply.github.com>
2021-07-16 09:49:31 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api',
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/grampsmd',
-    CLIENT_ORIGIN: 'https://grampsmdclient-acnyrxt0o.now.sh'
+    CLIENT_ORIGIN: 'https://grampsmdclient-iljjlckxv.now.sh/'
   }
